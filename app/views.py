f124dff3302edca392698c1512ce8da1a3702bad
mharesh <hareshchowdary7997gmail.com>
2023-08-10 22:22:44 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def bs_download(request):
+    return render(request,'bs_download.html')
+def bs_download1(request):
+    return render(request,'bs_download1.html')
